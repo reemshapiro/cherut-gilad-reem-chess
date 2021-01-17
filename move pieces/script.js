33c02b1d2bdf5af653ae12c6a246e1e11ac9551e
cherut gur <cherut.gur@gmail.com>
2021-01-17 16:24:04 +0200

    22

--- move pieces/script.js
+++ move pieces/script.js
@@ -1,5 +1,6 @@
-function movePiece(selectedPiece,selectedLocation){
+// function movePiece(selectedPiece,selectedLocation){
 
 
-}
-document.getElementById("1,1").classList.add=".pawn"
+// }
+
+  document.getElementById("1,1").classList.add("pawn");
