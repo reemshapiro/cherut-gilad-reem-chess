f41c5de1394aceef86d06cdab08e30bf73a43ab6
reem <reemshapiro@gmail.com>
2021-01-17 18:15:26 +0200

    cont

--- move pieces/dist/script.dev.js
+++ move pieces/dist/script.dev.js
@@ -1,13 +1,17 @@
 "use strict";
 
 var selectedLocation = "4,4";
-var selectedPiece;
+var selectedPiece = '4,4';
 
 function movePiece(event, selectedPiece, selectedLocation) {
-  console.log(event.target.id);
-  document.getElementById('4,4').classList.add("pawn");
-  document.getElementById("1,1").classList.remove("pawn");
+  selectedLocation = event.target.id;
+  document.getElementById(selectedLocation).innerHTML += "<div class=\"pawn\" id=".concat(selectedLocation, " onclick=\"selectPiece(event)\"></div>");
+  document.getElementById(selectedPiece).innerHTML = "";
   console.log(2559);
+}
+
+function selectPiece(event) {
+  selectedPiece = event.target.id;
 }
 
 document.getElementById("1,1").classList.add("pawn"); // movePiece(selectedPiece,selectedLocation)
--- move pieces/script.js
+++ move pieces/script.js
@@ -1,12 +1,15 @@
 let selectedLocation="4,4"
-let selectedPiece
+let selectedPiece='4,4'
 
 function movePiece(event,selectedPiece,selectedLocation){
-  console.log(event.target.id)
-    document.getElementById('4,4').classList.add("pawn");
-    document.getElementById("1,1").classList.remove("pawn");
+    selectedLocation=event.target.id
+    document.getElementById(selectedLocation).innerHTML+= `<div class="pawn" id=${selectedLocation} onclick="selectPiece(event)"></div>`
+    document.getElementById(selectedPiece).innerHTML="";
 console.log(2559)
 }
-
+function selectPiece(event){
+    selectedPiece=event.target.id
+}
 document.getElementById("1,1").classList.add("pawn");
+
 // movePiece(selectedPiece,selectedLocation)
