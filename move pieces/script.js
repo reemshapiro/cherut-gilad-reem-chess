fb0620a18968cadb6f410545ba901353b5859c00
Tal <tal.yaron@gmail.com>
2021-01-21 20:35:45 +0200

    fix

--- move pieces/script.js
+++ move pieces/script.js
@@ -31,20 +31,21 @@ function createBoard() {
 
 function setPiecesStartPosition() {
   piecesArr.forEach(piece => {
-    setPieceLocation(`${piece.position.i},${piece.position.j}`, piece.name, piece.icon,piece.type);
+    setPieceLocation(`${piece.position.i},${piece.position.j}`, piece.name, piece.icon, piece.type);
   })
 }
 
-const setPieceLocation = (selectedLocation, name, icon,type) => {
+const setPieceLocation = (selectedLocation, name, icon, type) => {
   document.getElementById(
     selectedLocation
   ).innerHTML += `<div name='${name}'  icon='${icon}' type='${type}' class="pawn" id=${selectedLocation} onclick="selectPiece(event)">${icon}</div>`;
 };
 
 
- const selectPiece = async  (event) => {
+function selectPiece(event) {
   selectedPiece = event.target.id;
-  selectedPieceName = event.target.name;
+  selectedPieceName = event.target.attributes[0].value;
+ 
   selectedPiece = selectedPiece.split(',')
   // console.log(typeof selectedPiece)
   // console.log(selectedPiece)
@@ -56,24 +57,24 @@ const setPieceLocation = (selectedLocation, name, icon,type) => {
   icon = event.target.attributes[1].value;
   type = event.target.attributes[2].value
   // console.log({x:parseInt(selectedPiece[0], 10),y:parseInt(selectedPiece[1], 10)})
-  let objectifier = {i:selectedPiece[0],j:selectedPiece[1]}
+  let objectifier = { i: selectedPiece[0], j: selectedPiece[1] }
   console.log(objectifier)
   // let optionalmovements =  init(type,{x:parseInt(selectedPiece[0], 10),y:parseInt(selectedPiece[1], 10)})
-  let optionalmovements =  checkOptionalmovements(type,{x:parseInt(selectedPiece[0], 10),y:parseInt(selectedPiece[1], 10)})
+  let optionalmovements = checkOptionalmovements(type, { x: parseInt(selectedPiece[0], 10), y: parseInt(selectedPiece[1], 10) })
   console.log(optionalmovements)
   let clickedpiece = {}
   piecesArr.forEach(piece => {
-    if(piece.position.i==objectifier.i&&piece.position.j==objectifier.j){
+    if (piece.position.i == objectifier.i && piece.position.j == objectifier.j) {
       console.log(piece.position)
       console.log(objectifier)
-     clickedpiece = piece
+      clickedpiece = piece
     }
-    
+
 
   });
   console.log(clickedpiece)
-  
-  authenticatedMovements =movementAuthentication(optionalmovements, clickedpiece)
+
+  authenticatedMovements = movementAuthentication(optionalmovements, clickedpiece)
 
 
 
@@ -82,7 +83,7 @@ const setPieceLocation = (selectedLocation, name, icon,type) => {
     console.log(document.getElementById(`${move.i},${move.j}`))
     document.getElementById(`${move.i},${move.j}`).addEventListener('click', movePiece);
     document.getElementById(`${move.i},${move.j}`).style.backgroundColor = 'red';
-    
+
   });
 
 }
@@ -103,17 +104,20 @@ function movePiece(event) {
   isClicked = false;
   selectedPiece = '';
 
-  authenticatedMovements.forEach(move =>{
+  authenticatedMovements.forEach(move => {
     document.getElementById(`${move.i},${move.j}`).removeEventListener('click', movePiece);
     document.getElementById(`${move.i},${move.j}`).style.backgroundColor = '';
   })
 
   console.log(newSL)
-  piecesArr.forEach(piece=>{
-    if(selectedPieceName == piece.name){
-      piece.position = {
-        i: parseInt(newSL[0],10) ,
-        j: parseInt(newSL[1],10)
+  piecesArr.map((piece, index) => {
+    console.log(selectedPieceName, piece.name)
+    if (selectedPieceName == piece.name) {
+     
+      piecesArr[index].position = {
+
+        i: parseInt(newSL[0], 10),
+        j: parseInt(newSL[1], 10)
       }
     }
   })
